5ff5818febda6ee38279aa1ff9b24310502a0d2d
鲨小鱼 <9542762+cgd-cgd@user.noreply.gitee.com>
2022-12-05 18:00:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <div class="a">234</div>
+    <el-button>123</el-button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped lang="less">
+.a {
+  color: #000;
+}
+</style>
